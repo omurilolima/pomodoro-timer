da778a9feb0d2ea93571c7ec16cd9cdee29947f3
Murilo Lima <murilo.serafim@clin.digital>
2026-03-03 19:28:59 +0000

    feat: Add action buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,25 +2,69 @@ import { useNavigation } from "@react-navigation/native";
 import { Text, TouchableOpacity, View } from "react-native";
 import { TNavigationScreenProps } from "../appRoutes";
 import { Theme } from "../shared/themes/Theme";
+import { StyleSheet } from "react-native";
 
 export const Home = () => {
   const navigation = useNavigation<TNavigationScreenProps>();
 
   return (
-    <View style={{ backgroundColor: Theme.colors.background }}>
-      <Text
-        style={{
-          fontFamily: Theme.fonts.interRegular,
-          fontSize: Theme.fontSizes.title,
-          color: Theme.colors.text,
-        }}
-      >
-        Home Page
-      </Text>
+    <View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.primaryButton}>
+          <Text style={styles.primaryButtonText}>Iniciar</Text>
+        </TouchableOpacity>
+      </View>
 
-      <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
-        <Text style={{ color: Theme.colors.text }}>Settings</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.primaryButton}>
+          <Text style={styles.primaryButtonText}>Pausar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.secondaryButton}>
+          <Text style={styles.secondaryButtonText}>Parar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.primaryButton}>
+          <Text style={styles.primaryButtonText}>Continuar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.secondaryButton}>
+          <Text style={styles.secondaryButtonText}>Reiniciar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  primaryButton: {
+    backgroundColor: Theme.colors.primary,
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+    borderRadius: 55,
+  },
+  primaryButtonText: {
+    color: Theme.colors.text,
+    fontSize: Theme.fontSizes.body,
+    fontFamily: Theme.fonts.interRegular,
+  },
+  secondaryButton: {
+    borderColor: Theme.colors.primary,
+    borderWidth: 2,
+    borderRadius: 55,
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+  },
+  secondaryButtonText: {
+    color: Theme.colors.text,
+    fontSize: Theme.fontSizes.body,
+    fontFamily: Theme.fonts.interRegular,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 16,
+  },
+});
