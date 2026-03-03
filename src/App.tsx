e8623f052d1e2f79234a1ae7260b0dc18b5d8215
Murilo Lima <murilo.serafim@clin.digital>
2026-03-03 18:51:02 +0000

    feat: safe area view config

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
-import { View } from 'react-native';
-import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
-import * as SplashScreen from 'expo-splash-screen';
-import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_700Bold } from "@expo-google-fonts/inter";
+import { useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
 
-import { Home } from './pages/Home';
-import { useEffect } from 'react';
-import { AppRoutes } from './appRoutes';
+import { AppRoutes } from "./appRoutes";
+import { Theme } from "./shared/themes/Theme";
+import { StatusBar } from "expo-status-bar";
 
 SplashScreen.preventAutoHideAsync();
 
 export function App() {
-  
-  const [ loaded, error ] = useFonts({
+  const [loaded, error] = useFonts({
     InterRegular: Inter_400Regular,
     InterBold: Inter_700Bold,
-  })
+  });
 
-  useEffect( () => {
-    if ( loaded || error ) {
+  useEffect(() => {
+    if (loaded || error) {
       SplashScreen.hide();
     }
-  }, [loaded, error] )
+  }, [loaded, error]);
 
-  if ( !loaded && !error ) return
-  
-  return <AppRoutes />
+  if (!loaded && !error) return;
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: Theme.colors.background }}>
+      <StatusBar style="light" />
+      <AppRoutes />
+    </SafeAreaView>
+  );
 }
